5d1d7eee5c96d7b91faea9e1d62a6ee580fb01fa
Reih02 <reillyhaskins@gmail.com>
2022-07-31 20:42:32 +1200

    changed name of port variable array

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 def server(port_1, port_2, port_3):
-    vars = [port_1, port_2, port_3]
-    if len(set(vars)) != len(vars):
-        # not all unique values
+    port_vars = [port_1, port_2, port_3]
+    if len(set(port_vars)) != len(port_vars):
+        # not all unique port values
         print("All port numbers must be unique!")
         quit()
-    elif not all(x >= 1024 and x <= 64000 for x in vars):
+    elif not all(x >= 1024 and x <= 64000 for x in port_vars):
+        # checks if any port values are less than 1024 or greater than 64000
         print("All port numbers must be between 1024 and 64000!")
         quit()
 
