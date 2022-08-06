14d8f232b7f04ceabd05253f182f03ef13292d0d
Reih02 <reillyhaskins@gmail.com>
2022-08-06 13:57:54 +1200

    Added socket binding and sketched out a code block for listening on the sockets

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+import select
+from socket import *
+
 def server(port_1, port_2, port_3):
     port_vars = [port_1, port_2, port_3]
     if len(set(port_vars)) != len(port_vars):
@@ -9,5 +12,37 @@ def server(port_1, port_2, port_3):
         print("All port numbers must be between 1024 and 64000!")
         quit()
 
+    try:
+        # creates three UDP sockets using IPv4
+        server_socket_1 = socket(AF_INET, SOCK_DGRAM)
+        server_socket_2 = socket(AF_INET, SOCK_DGRAM)
+        server_socket_3 = socket(AF_INET, SOCK_DGRAM)
 
-print(server(1890, 1510, 64001))
+        # binds said sockets to the three port numbers
+        server_socket_1.bind(('localhost', port_1))
+        server_socket_2.bind(('localhost', port_2))
+        server_socket_3.bind(('localhost', port_3))
+    except:
+        print("Socket binding failed.")
+        quit()
+
+    # listen for incoming packets on the sockets
+    input_sockets = [server_socket_1, server_socket_2, server_socket_3]
+    output_sockets = []
+    while True:
+        readable, writable, exceptional = select.select(input_sockets, output_sockets, input_sockets)
+        for r in readable:
+            if r in input_sockets:
+                # packet received
+                receiving_socket = r # used for knowing which language client wants
+                conn, client_addr = r.accept()
+                print(client_addr)
+                # receives packet with buffer size of 1024
+                client_packet = r.recvfrom(1024)
+                received_packet = bytearray(client_packet)
+                print(received_packet)
+
+                # packet checks
+
+
+print(server(1890, 1510, 6390))
