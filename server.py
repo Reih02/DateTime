3465de9d53569ea35fdc8664209fa22d50e42f12
Reih02 <reillyhaskins@gmail.com>
2022-08-10 12:24:19 +1200

    Added function to create textual representation of DT-Response text

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 import select
 import time
-from datetime import date
 from socket import *
 
 def server(port_1, port_2, port_3):
@@ -37,11 +36,10 @@ def server(port_1, port_2, port_3):
             # incoming data on socket
             if r in input_sockets:
                 # packet received
-                receiving_socket = r # used for knowing which language client wants
+                receiving_socket = input_sockets.index(r) + 1 # used for knowing which language client wants
                 client_packet = r.recvfrom(1024) # receives packet with buffer size of 1024
                 message, client_address = client_packet
                 received_packet = bytearray(message)
-                print(received_packet)
 
                 # packet checks
                 if len(received_packet) != 6:
@@ -65,11 +63,56 @@ def server(port_1, port_2, port_3):
                 elif ((received_packet[4] << 8) + received_packet[5]) == 0x0002:
                     request_type = "time"
 
-                curr_date = date.today().strftime("%Y : %m : %d")
-                curr_time = time.strftime("%H : %M") 
+                if receiving_socket == 1:
+                    chosen_language = "english"
+                elif receiving_socket == 2:
+                    chosen_language = "maori"
+                elif receiving_socket == 3:
+                    chosen_language = "german"
+
+                # prepare DT-Response packet
+                text = textual_repr(chosen_language, request_type)
 
 
+def textual_repr(language, request_type):
 
+    months = {"January": ("Kohitatea", "Januar"),
+              "February": ("Hui-tanguru", "Februar"),
+              "March": ("Poutu-te-rangi", "März"),
+              "April": ("Paenga-whawha", "April"),
+              "May": ("Haratua", "Mai"),
+              "June": ("Pipiri", "Juni"),
+              "July": ("Hongongoi", "Juli"),
+              "August": ("Here-turi-koka", "August"),
+              "September": ("Mahuru", "September"),
+              "October": ("Whiringa-a-nuku", "Oktober"),
+              "November": ("Whiringa-a-rangi", "November"),
+              "December": ("Hakihea", "Dezember")}
+
+    if language == "english":
+        if request_type == "date":
+            text = time.strftime("Today's date is %B %d, %Y")
+
+        elif request_type == "time":
+            text = time.strftime("The current time is %H:%M")
+
+    elif language == "maori":
+        if request_type == "date":
+            eng_month = time.strftime("%B")
+            text = time.strftime("Ko te ra o tenei ra ko {} %d, %Y").format(months[eng_month][0])
+
+        elif request_type == "time":
+            text = time.strftime("Ko te wa o tenei wa %H:%M")
+
+    elif language == "german":
+        if request_type == "date":
+            eng_month = time.strftime("%B")
+            text = time.strftime("Heute ist der %d. {} %Y").format(months[eng_month][1])
+
+        elif request_type == "time":
+            text = time.strftime("Die Uhrzeit ist %H:%M")
+
+    return text
 
 
 print(server(1890, 1510, 6390))
