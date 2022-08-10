10554e8400d1425421868d0c95804d9e4dc06962
Reih02 <reillyhaskins@gmail.com>
2022-08-10 16:38:57 +1200

    added functionality in client.py to prepare and send DT-request packet to server

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
 import sys
 from socket import *
 
+MAGICNO = 0x497E
+
 def client(request_type, address, port_num):
     if request_type != "date" and request_type != "time":
         sys.exit("Request type invalid")
@@ -14,9 +16,29 @@ def client(request_type, address, port_num):
         # checks if any port values are less than 1024 or greater than 64000
         sys.exit("Port number must be between 1024 and 64000!")
 
-    serverAddress = (address, port_num)
+    server_address = (address, port_num)
 
-    #client_socket
+    # opens client socket
+    client_socket = socket.socket(AF_INET, SOCK_DGRAM)
+
+    # prepares and sends DT-Request packet to server
+    if request_type == "date":
+        request_type = 0x0001
+    elif request_type == "time":
+        request_type = 0x0002
+
+    magic_number = MAGICNO.to_bytes(2, 'big')
+    packet_type = 0x0001.to_bytes(2, 'big')
+    request_type = request_type.to_bytes(2, 'big')
+
+    bytes = magic_number + packet_type + request_type
+
+    request_packet = bytearray(list(bytes))
+
+    client_socket.sendto(request_packet, server_address)
+
+
+
 
 
 
