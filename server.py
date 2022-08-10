fed21e359334ec98143027ad24b3a27a29a2d3ff
Reih02 <reillyhaskins@gmail.com>
2022-08-10 14:16:31 +1200

    added code to create the DT-Response packet and send it back to the client on the receiving socket

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ import select
 import time
 from socket import *
 
+MAGICNO = 0x497E
+
 def server(port_1, port_2, port_3):
     port_vars = [port_1, port_2, port_3]
     if len(set(port_vars)) != len(port_vars):
@@ -45,7 +47,7 @@ def server(port_1, port_2, port_3):
                 if len(received_packet) != 6:
                     print("Length of packet not 6 bytes")
                     break
-                if ((received_packet[0] << 8) + received_packet[1]) != 0x497E:
+                if ((received_packet[0] << 8) + received_packet[1]) != MAGICNO:
                     print("MagicNo field incorrect")
                     break
                 if ((received_packet[2] << 8) + received_packet[3]) != 0x0001:
@@ -65,16 +67,52 @@ def server(port_1, port_2, port_3):
 
                 if receiving_socket == 1:
                     chosen_language = "english"
+                    language_code = 0x0001
                 elif receiving_socket == 2:
                     chosen_language = "maori"
+                    language_code = 0x0002
                 elif receiving_socket == 3:
                     chosen_language = "german"
+                    language_code = 0x0003
 
                 # prepare DT-Response packet
                 text = textual_repr(chosen_language, request_type)
+                if len(text) > 255:
+                    print("Text too long")
+                    break
+
+                # initialise all current date/time values
+                year, month, day, hour, minute = date_time_info()
+
+                # maybe need a buffer??
+
+                # put values into corresponding amount of bytes for packet
+                magic_number = MAGICNO.to_bytes(2, 'big')
+                packet_type = 0x0002.to_bytes(2, 'big')
+                language_code = language_code.to_bytes(2, 'big')
+                year = year.to_bytes(2, 'big')
+                month = month.to_bytes(1, 'big')
+                day = day.to_bytes(1, 'big')
+                hour = hour.to_bytes(1, 'big')
+                minute = minute.to_bytes(1, 'big')
+                length = len(text).to_bytes(1, 'big')
+                text = text.to_bytes((text.bit_length() + 7) // 8, 'big')
+
+                bytes = magic_number + packet_type + language_code + \
+                        year + month + day + hour + minute + length + text
+
+                # create response packet as a bytearray from bytes
+                response_packet = bytearray(list(bytes))
+
+                # send response packet on receiving socket
+                r.sendto(response_packet, client_address)
+
+
 
 
 def textual_repr(language, request_type):
+    '''Gets appropriate textual representation for corresponding language
+       and request type'''
 
     months = {"January": ("Kohitatea", "Januar"),
               "February": ("Hui-tanguru", "Februar"),
@@ -112,7 +150,34 @@ def textual_repr(language, request_type):
         elif request_type == "time":
             text = time.strftime("Die Uhrzeit ist %H:%M")
 
-    return text
+    return bytearray(text.encode())
 
+
+def date_time_info():
+    '''gets current date/time values and converts into integers for use in
+       response packet'''
+
+    year = int(time.strftime("%Y"))
+    month = time.strftime("%m")
+    if month[0] == '0':
+        month = month[1:]
+    month = int(month)
+
+    day = time.strftime("%d")
+    if day[0] == '0':
+        day = day[1:]
+    day = int(day)
+
+    hour = time.strftime("%H")
+    if hour[0] == '0':
+        hour = hour[1:]
+    hour = int(hour)
+
+    minute = time.strftime("%M")
+    if minute[0] == '0':
+        minute = minute[1:]
+    minute = int(minute)
+
+    return (year, month, day, hour, minute)
 
 print(server(1890, 1510, 6390))
